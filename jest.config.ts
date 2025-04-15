91de92f0d1b6a9bb6cdef22f6753d044fb0fd001
LeonardoHRezende <rezendeleo64@gmail.com>
2025-04-14 21:44:12 -0300

    feat: create tests

---
+++ jest.config.ts
@@ -0,0 +1,24 @@
+const config = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  moduleFileExtensions: ['js', 'json', 'ts'],
+  rootDir: 'src',
+  testRegex: '.*\\.spec\\.ts$',
+  transform: {
+    '^.+\\.(t|j)s$': 'ts-jest',
+  },
+  collectCoverage: true,
+  coverageDirectory: '../coverage',
+  coverageProvider: 'v8',
+  collectCoverageFrom: ['**/*/*.usecase.ts'],
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: 80,
+    },
+  },
+};
+
+export default config;
